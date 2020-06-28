98a6b2bc37ebe89ece7e4692996ba23287b94bd2
cto_vugha <cto@vugha.com>
2020-06-28 10:51:58 +0530

    WIP

---
+++ hack20/lib/screens/ngoScreen/ngoScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class NgoScreen extends StatefulWidget {
+  @override
+  _NgoScreenState createState() => _NgoScreenState();
+}
+
+class _NgoScreenState extends State<NgoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('ngo screen');
+  }
+}
---
+++ hack20/lib/services/auth.dart
@@ -0,0 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hack20/models/userModel.dart';
+import 'package:hack20/services/roleVerification.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null
+        ? User(
+            uid: user.uid,
+            email: user.email,
+            isUserEmailVerified: user.isEmailVerified)
+        : null;
+  }
+
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      await AccountVerification().addSingleRole(email, 'USER');
+      return _userFromFirebaseUser(user);
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+  Future ngoRegisterWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      await AccountVerification().addSingleRole(email, 'NGO');
+      return _userFromFirebaseUser(user);
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
+  Future resetPassword(String email) async {
+    try {
+      return await _auth.sendPasswordResetEmail(email: email);
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+}
